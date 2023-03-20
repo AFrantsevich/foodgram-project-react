c546188ccd48e57c83891dbc3a553e6f4d23164e
unknown <Andrey.Frantsevich@gmail.com>
2023-03-20 23:39:01 +0300

    Final.1.1

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,7 +10,9 @@ v1_router = routers.DefaultRouter()
 v1_router.register(r'tags', TagsViewSet)
 v1_router.register(r'recipes', RecipeViewSet)
 v1_router.register(r'users/subscriptions', SubscriptionsViewSet)
-v1_router.register(r'users/(?P<id>\d+)/subscribe', SubscriptionsViewSet)
-v1_router.register(r'recipes/(?P<id>\d+)/favorite', FavoriteViewSet)
+v1_router.register(r'users/(?P<id>\d+)/subscribe', SubscriptionsViewSet,
+                   basename='subscribe')
+v1_router.register(r'recipes/(?P<id>\d+)/favorite', FavoriteViewSet,
+                   basename='favorite')
 v1_router.register(r'recipes/(?P<id>\d+)/shopping_cart', CartViewSet)
 v1_router.register(r'ingredients', IngredientsViewSet)
